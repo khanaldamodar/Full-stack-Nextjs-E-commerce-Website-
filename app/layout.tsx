49f687a04fc889c23ca21ed8572f37c22fd6cedc
Deepak Khanal <deepakkhanal931@gmail.com>
2025-10-30 16:05:14 +0545

    Documented apis and implemented husky

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased bg-[#AEC958]`}
+        className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased`}
         >
       
         {children}
