35707050aed87d728f57d16a5c932ea5712d96b6
namrata <namrataap725@gmail.com>
2025-10-31 16:29:34 +0545

    login page

--- app/(root)/login/page.tsx
+++ app/(root)/login/page.tsx
@@ -47,7 +47,7 @@ export default function Page() {
 
   return (
     <div className="flex h-screen w-full items-center justify-center bg-linear-to-b from-green-500 to-blue-500">
-      <div className="w-full max-w-md bg-white rounded-2xl shadow-xl p-8">
+      <div className=" mt-23 w-full max-w-md bg-white rounded-2xl shadow-xl p-8">
         <div className="flex flex-col items-center">
           <Image
             src="/logo.jpeg"
