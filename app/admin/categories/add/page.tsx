5f592d9e42828098170997e9880ce5b0c1fe42ae
namrata <namrataap725@gmail.com>
2025-10-31 13:30:42 +0545

    added add category

---
+++ app/admin/categories/add/page.tsx
@@ -0,0 +1,36 @@
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center mx-auto ">
+        <div className="flex-col flex items-center justify-center mt-3 gap-6 bg-zinc-800 w-2/5 rounded p-5">
+      <h1 className="text-2xl text-white font-bold text-center mb-2 pt-4">Add Category</h1>
+      <form className=" ">
+        <div className="rounded-xl p-2 gap-4 shadow-lg flex justify-center items-center mx-auto ">
+          
+                <div className="flex-col flex gap-4">
+                <label htmlFor="categoryName" className="text-xl">Category Name</label>
+                <input 
+                type="text" 
+                id="productName" 
+                name="productName" 
+                required 
+                className="p-3 bg-zinc-900 text-white placeholder-zinc-400 rounded-2xl outline-none focus:ring-2 focus:ring-zinc-900" 
+                placeholder="Product Name"
+                />
+                </div>
+
+
+        </div>
+        <div className="flex justify-center mx-auto items-center pb-2">
+            <button 
+          type="submit" 
+          className="mt-4 bg-white hover:bg-zinc-100 text-black cursor-pointer font-bold py-3 rounded-2xl transition-all duration-200 w-full"
+        >
+          Add
+        </button>
+        </div>
+        
+      </form>
+    </div>
+    </div>
+  );
+}
--- app/admin/categories/page.tsx
+++ app/admin/categories/page.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import React from 'react'
+import { useRouter } from "next/navigation";
+
 
 const page = () => {
+      const router = useRouter();
+
   return (
     <div className='h-screen bg-red-500 '>
         {/* header */}
         <div className='w-full bg-zinc-700 flex items-center p-4 shadow-md justify-between '>
-            <h3 className='text-2xl text-white font-bold'>Categories</h3>
-            <button className='bg-zinc-600 text-white px-4 py-2  hover:bg-zinc-700 transition '>Add Category</button>
+            <h3 className='text-2xl text-white font-bold'>Category</h3>
+            <button onClick={() => router.push("/admin/categories/add")} className='bg-zinc-600 text-white px-4 py-2  hover:bg-zinc-700 transition '>Add Category</button>
         </div>
         
     </div>
