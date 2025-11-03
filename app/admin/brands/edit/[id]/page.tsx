0ba96fa875b0cd48457ea717720c8a3ed5aec38c
namrata <namrataap725@gmail.com>
2025-11-03 16:30:48 +0545

    update ui of form for add and update

--- app/admin/brands/edit/[id]/page.tsx
+++ app/admin/brands/edit/[id]/page.tsx
@@ -54,7 +54,7 @@ export default function EditBrandPage() {
 
   return (
     <div className="flex items-center justify-center mx-auto">
-      <div className="flex-col flex items-center justify-center mt-3 gap-6 bg-zinc-800 w-2/5 rounded p-5">
+      <div className="flex-col flex items-center justify-center mt-3 gap-6 bg-linear-to-t from-green-700 to-green-400 w-2/5 rounded p-5">
         <h1 className="text-2xl text-white font-bold text-center mb-2 pt-4">Update Brand</h1>
         <form className="w-full" onSubmit={handleSubmit}>
           <div className="rounded-xl p-2 gap-4 shadow-lg flex justify-center items-center mx-auto w-full">
@@ -67,7 +67,7 @@ export default function EditBrandPage() {
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 required
-                className="p-3 bg-zinc-900 text-white placeholder-zinc-400 rounded-2xl outline-none focus:ring-2 focus:ring-zinc-900 w-full"
+                className="p-3 bg-green-900 text-white placeholder-zinc-400 rounded-2xl outline-none focus:ring-2 focus:ring-green-700 w-full"
               />
             </div>
           </div>
@@ -76,7 +76,7 @@ export default function EditBrandPage() {
             <button
               type="submit"
               disabled={updating}
-              className="mt-4 bg-white hover:bg-zinc-100 text-black cursor-pointer font-bold py-3 rounded-2xl transition-all duration-200 w-full"
+              className="mt-4 bg-white hover:bg-green-300 text-black cursor-pointer font-bold py-3 rounded-2xl transition-all duration-200 w-full"
             >
               {updating ? "Updating..." : "Update"}
             </button>
