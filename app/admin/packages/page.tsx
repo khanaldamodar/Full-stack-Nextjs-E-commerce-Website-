e39986b49eb9364a2f08fb4d94b6294c4bc676a7
namrata <namrataap725@gmail.com>
2025-11-04 12:23:00 +0545

    packages-page

--- app/admin/packages/page.tsx
+++ app/admin/packages/page.tsx
@@ -1,27 +1,70 @@
 "use client";
 
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import CRUDTable from "@/components/admin-components/CRUDTable";
 
-const page = () => {
-      const router = useRouter();
-
-  return (
-    <div className='h-screen bg-red-500 '>
-        {/* header */}
-        <div className='w-full bg-zinc-700 flex items-center p-4 shadow-md justify-between '>
-            <h3 className='text-2xl text-white font-bold'>Packages</h3>
-            <button onClick={() => router.push("/admin/packages/add")} className='bg-zinc-600 text-white px-4 py-2  hover:bg-zinc-700 transition '>Add Package</button>
-        </div>
-        
-        <div>
-          <div>
-                      <CRUDTable endpoint="packages" columns={["id"]} />
-                   </div>
-        </div>
-    </div>
-  )
+interface PackageType {
+  id: number;
+  name: string;
 }
 
-export default page;
+export default function PackagesPage(){
+  const router = useRouter();
+  const [packages, setPackages] = useState<PackageType[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+
+  useEffect(() => {
+    const fetchPackages = async () => {
+      try {
+        const res = await fetch("/api/packages");
+        if (!res.ok) throw new Error("Failed to fetch Packages");
+        const data: PackageType[] = await res.json();
+        setPackages(data);
+      } catch (err: any) {
+        setError(err.message || "Something went wrong");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPackages();
+  }, []);
+
+  if (loading) return <div className="p-6 text-lg">Loading Packages...</div>;
+  if (error) return <div className="p-6 text-red-500">Error: {error}</div>;
+
+  return (
+    <div className="h-screen flex flex-col">
+      {/* header */}
+      <div className="w-full bg-white flex items-center p-4 shadow-md justify-between">
+        <h3 className="text-2xl text-black font-bold">Packages</h3>
+        <button
+          onClick={() => router.push("/admin/packages/add")}
+          className="bg-green-600 text-white px-4 py-2 hover:bg-green-100 transition"
+        >
+          Add Packages
+        </button>
+      </div>
+
+      {/* main content */}
+      <div className="flex-1 p-4">
+        {packages.length > 0 ? (
+          <CRUDTable
+            endpoint="packages"
+            columns={["name"]}
+            data={packages}
+            setData={setPackages} 
+          />
+        ) : (
+          <div className="text-center text-gray-400 mt-10">
+            No packages found.
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+
