dce10aa5fcc4349a4895c9ba89aa63b55787ef39
namrata <namrataap725@gmail.com>
2025-11-09 11:45:25 +0545

    dashboard-UI

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client";
+import React from "react";
+import Card from "@/components/admin-components/Card";
+const stats = [
+  { label: "Brands", value: 1726 },
+  { label: "Product", value: 1726 },
+  { label: "Packages", value: 1726 },
+  { label: "jnfiwuefnuwef", value: 1726 },
+  { label: "jnfiwuefnuwef", value: 1726 },
+];
 
-const page = () => {
+const Page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="min-h-screen p-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+        {stats.map((item, index) => (
+          <Card key={index} label={item.label} value={item.value} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default Page;
